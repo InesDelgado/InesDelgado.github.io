517a2ca5a94ef386d79bda6a5776fb2b6f8b2e99
InesDelgado <inesdelgadohernandez1977@gmail.com>
2023-10-05 14:35:53 +0100

    hmtl js modificed

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// RECIBIR EMAILS (Con EmailJs): 
+// RECIBIR EMAILS (Con EmailJs):
 const btn = document.getElementById('button');
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-   btn.value = 'Sending...';
+  btn.value = 'Sending...';
 
-   const serviceID = 'default_service';
-   const templateID = 'template_xr8h2tz';
+  const serviceID = 'default_service';
+  const templateID = 'template_xr8h2tz';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
+  emailjs.sendForm(serviceID, templateID, this).then(
+    () => {
       btn.value = 'Send Email';
       alert('Sent!');
-    }, (err) => {
+    },
+    (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
-    });
+    }
+  );
 });
-//para cerrar la pestaña de busqueda en dispositivo movil
+
+// Para cerrar la pestaña de búsqueda en dispositivo móvil
 $(document).ready(function() {
-
   $(".navbar-nav a.nav-link").on("click", function() {
-
     var target = $(this).attr("href");
 
     // Desplazarse a la sección correspondiente con animación
@@ -36,6 +36,7 @@ $(document).ready(function() {
     $("#navbarNav").removeClass("show");
   });
 });
+
 // Agrega una animación al hacer clic en los elementos de la barra de navegación
 $(document).ready(function() {
   $('.nav-link').click(function() {
@@ -45,66 +46,120 @@ $(document).ready(function() {
     }, 1000); // Cambia esto según la duración de tu animación
   });
 });
-document.addEventListener('DOMContentLoaded', function() {
-  // Obtén todos los botones con la clase btn-bounce
-  const bounceButtons = document.querySelectorAll('.btn-bounce');
 
-  // Agrega un event listener para cada botón
-  bounceButtons.forEach(function(button) {
-    button.addEventListener('click', function() {
-      // Remueve la clase btn-bounce
-      button.classList.remove('btn-bounce');
+// Obtén todos los botones con la clase btn-bounce
+const bounceButtons = document.querySelectorAll('.btn-bounce');
 
-      // Agrega la clase btn-bounce después de un pequeño retraso
-      setTimeout(function() {
-        button.classList.add('btn-bounce');
-      }, 10);
-    });
+// Agrega un event listener para cada botón
+bounceButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Remueve la clase btn-bounce
+    button.classList.remove('btn-bounce');
+
+    // Agrega la clase btn-bounce después de un pequeño retraso
+    setTimeout(function() {
+      button.classList.add('btn-bounce');
+    }, 10);
   });
 });
-  // Selecciona el enlace de "Contacto" en la barra de navegación
-  const contactoLink = document.querySelector('a[href="#contact"]');
 
-  // Selecciona el elemento <h3> en la sección de contacto
-  const contactHeading = document.getElementById('contact-heading');
+// Selecciona el enlace de "Contacto" en la barra de navegación
+const contactoLink = document.querySelector('a[href="#contact"]');
 
-  // Agrega un evento de clic al enlace de "Contacto"
-  contactoLink.addEventListener('click', (event) => {
-    // Evita que el enlace se comporte como un enlace normal
-    event.preventDefault();
+// Selecciona el elemento <h3> en la sección de contacto
+const contactHeading = document.getElementById('contact-heading');
 
-    // Agrega la clase CSS para la animación (utilizando anime.js)
-    anime({
-      targets: contactHeading,
-      translateX: [
-        { value: '-10px', duration: 100, easing: 'easeInOutSine' },
-        { value: '10px', duration: 100, easing: 'easeInOutSine' },
-        { value: '0', duration: 100, easing: 'easeInOutSine' }
-      ],
-      loop: 6, // Repite la animación 6 veces
-    });
+// Agrega un evento de clic al enlace de "Contacto" para iniciar la animación
+contactoLink.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // Variable para contar las repeticiones de la animación
+  let animationCount = 0;
+
+  // Función para iniciar la animación de rebote
+  function startBounceAnimation() {
+    if (animationCount < 6) {
+      anime({
+        targets: contactHeading,
+        translateY: [
+          { value: -10, duration: 100, easing: 'easeInOutSine' },
+          { value: 10, duration: 100, easing: 'easeInOutSine' },
+          { value: 0, duration: 100, easing: 'easeInOutSine' }
+        ],
+        complete: function() {
+          animationCount++;
+
+          // Si se completaron las 6 repeticiones, detener la animación
+          if (animationCount >= 6) {
+            isAnimating = false;
+          } else {
+            // Iniciar la siguiente repetición
+            startBounceAnimation();
+          }
+        }
+      });
+    }
+  }
+
+  // Iniciar la primera repetición
+  startBounceAnimation();
+});
+
+// Agrega un evento de clic al enlace "Sobre Mí" en la barra de navegación
+const sobreMiLink = document.querySelector('a.sobre-mi-link');
+
+sobreMiLink.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  // Selecciona los elementos dentro de la sección "Sobre Mí" a los que deseas aplicar el efecto de rebote
+  const sobreMiElements = document.querySelectorAll('.animate__animated');
+
+  // Elimina la clase de animación actual de los elementos
+  sobreMiElements.forEach((element) => {
+    element.classList.remove('animate__animated', 'animate__bounce');
   });
-  document.addEventListener('DOMContentLoaded', function() {
-    // Obtén el enlace de "Inicio" por su id
-    const inicioLink = document.getElementById('inicio-link');
-  
-    // Agrega un evento de clic al enlace de "Inicio"
-    inicioLink.addEventListener('click', function(event) {
-      // Evita que el enlace se comporte como un enlace normal y recargue la página
-      event.preventDefault();
-  
-      // Selecciona la cabecera por su clase CSS
-      const cabecera = document.querySelector('header');
-  
-      // Elimina la clase de animación actual
-      cabecera.classList.remove('animate__animated', 'animate__bounceInLeft');
-  
-      // Agrega la clase de animación nuevamente para reiniciarla
-      setTimeout(function() {
-        cabecera.classList.add('animate__animated', 'animate__bounceInLeft');
-      }, 10); // Puedes ajustar el tiempo según tu preferencia
+
+  // Agrega la clase de animación nuevamente para reiniciarla después de un breve retraso
+  setTimeout(function() {
+    sobreMiElements.forEach((element) => {
+      element.classList.add('animate__animated', 'animate__bounce');
     });
+  }, 45); // Puedes ajustar el tiempo según tu preferencia
+});
+
+// Agregar un evento de clic al enlace de "Inicio" para volver a la cabecera
+const inicioLink = document.getElementById('inicio-link');
+
+inicioLink.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  // Selecciona la cabecera por su clase CSS
+  const cabecera = document.querySelector('header');
+
+  // Elimina la clase de animación actual
+  cabecera.classList.remove('animate__animated', 'animate__bounceInLeft');
+
+  // Agrega la clase de animación nuevamente para reiniciarla
+  setTimeout(function() {
+    cabecera.classList.add('animate__animated', 'animate__bounceInLeft');
+  }, 10); // Puedes ajustar el tiempo según tu preferencia
+
+  // Desplaza la página a la cabecera
+  const headerOffset = cabecera.getBoundingClientRect().top + window.scrollY;
+  window.scrollTo({
+    top: headerOffset,
+    behavior: 'smooth' // Agrega un desplazamiento suave
   });
+});
+
+
+
+
+
+
+
+  
+  
   
 
 
